daa8e77674b328123b7def22305348ebb58bc726
tako0614 <shoutatomiyama0614@gmail.com>
2026-06-30 20:00:56 +0000

    Split mobile-kit Solid exports

--- mobile-kit/src/index.ts
+++ mobile-kit/src/index.ts
@@ -127,21 +127,6 @@ export {
   type BrowserNativeWindow,
 } from "./native.ts";
 export {
-  MobileClientShell,
-  type MobileClientShellProps,
-  type MobileShellCopy,
-  type MobileShellHostAction,
-  type MobileShellHostActionContext,
-  type MobileShellMetric,
-  type MobileShellNativeIntent,
-} from "./solid-shell.tsx";
-export {
-  MobileClientApp,
-  renderMobileClientApp,
-  type MobileClientAppProps,
-  type RenderMobileClientAppOptions,
-} from "./solid-app.tsx";
-export {
   createTauriMobilePushPluginAdapter,
   createTauriOpenerCallIntentAdapter,
   authenticateBiometric,
