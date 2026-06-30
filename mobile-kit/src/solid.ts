7530ac019d9c7e2dba71156b64be4bb119093836
tako0614 <shoutatomiyama0614@gmail.com>
2026-06-30 20:00:56 +0000

    Split mobile-kit Solid exports

---
+++ mobile-kit/src/solid.ts
@@ -0,0 +1,15 @@
+export {
+  MobileClientShell,
+  type MobileClientShellProps,
+  type MobileShellCopy,
+  type MobileShellHostAction,
+  type MobileShellHostActionContext,
+  type MobileShellMetric,
+  type MobileShellNativeIntent,
+} from "./solid-shell.tsx";
+export {
+  MobileClientApp,
+  renderMobileClientApp,
+  type MobileClientAppProps,
+  type RenderMobileClientAppOptions,
+} from "./solid-app.tsx";
